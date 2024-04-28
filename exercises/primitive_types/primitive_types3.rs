e37c63a68c57be68f3ff8cd05a77db6d895c912f
milkvewsl2 <vlanw@foxmail.com>
2024-04-29 02:07:09 +0800

    update

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,14 +5,15 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let a = ???
+    let a = ["Are we there yet?"; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
+    print!("{} {}", a[0], a[99])
 }
