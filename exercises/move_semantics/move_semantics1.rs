5d652969b6347eb5611c73d80d62e68ee8550ca6
milkvewsl2 <vlanw@foxmail.com>
2024-04-30 02:11:46 +0800

    update

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -3,26 +3,29 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
+use std::cell::RefCell;
+use std::rc::Rc;
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Rc::new(RefCell::new(Vec::new()));
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0.clone());
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec0", vec0.borrow().len(), vec0.borrow());
+    println!("{} has length {} content `{:?}`", "vec1", vec1.borrow().len(), vec1.borrow());
 
-    vec1.push(88);
+    vec1.borrow_mut().push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec1", vec1.borrow().len(), vec1.borrow());
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
-
-    vec
+fn fill_vec(vec: Rc<RefCell<Vec<i32>>>) -> Rc<RefCell<Vec<i32>>> {
+    {
+        let mut vec = vec.borrow_mut();
+        vec.push(22);
+        vec.push(44);
+        vec.push(66);
+    }
+    Rc::clone(&vec)
 }
--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -9,10 +9,67 @@
 
 // I AM NOT DONE
 
-fn main() {
-    let vec0 = Vec::new();
+// 1. You could make another, separate version of the data that's in `vec0` and pass that 
+// to `fill_vec` instead.
 
-    let mut vec1 = fill_vec(vec0);
+// fn main() {
+//     let vec0 = Vec::new();
+
+//     let vec0_copy = vec0.clone();
+//     let mut vec1 = fill_vec(vec0_copy);
+
+//     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
+
+//     vec1.push(88);
+
+//     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec;
+
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+
+//     vec
+// }
+
+// 2. Make `fill_vec` borrow its argument instead of taking ownership of it,
+// and then copy the data within the function (`vec.clone()`) in order to return an owned
+// `Vec<i32>`.
+
+// fn main() {
+//     let vec0 = Vec::new();
+
+//     let mut vec1 = fill_vec(&vec0);
+
+//     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
+
+//     vec1.push(88);
+
+//     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec.clone();
+
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+
+//     vec
+// }
+
+// 3. Or, you could make `fill_vec` *mutably* borrow a reference to its argument (which will need to be
+// mutable), modify it directly, then not return anything. This means that `vec0` will change over the
+// course of the function, and makes `vec1` redundant (make sure to change the parameters of the `println!`
+// statements if you go this route)
+
+fn main() {
+    let mut vec0 = Vec::new();
+
+    let mut vec1 = fill_vec(&mut vec0);
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
@@ -21,12 +78,12 @@ fn main() {
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec.to_vec()
 }
